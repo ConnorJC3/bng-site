ece982bdf910cb0cec6a84ffe0c740a9563d2456
Christopher Miller <admin@mctherealm.net>
2020-01-12 21:57:07 -0500

    Deployment fixes

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import App from 'next/app'
+import Head from 'next/head'
+import React from 'react'
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <>
+        <Head>
+          <title>Broken Neck Gang</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
+  }
+}
